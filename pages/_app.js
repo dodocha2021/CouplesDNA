87a1512a8a6428ab329eb3c35d52a67fa3792459
dodocha <dodocha@gmail.com>
2025-07-10 11:58:57 -0700

    V1.0.4

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
 import '../styles/animations.css';
 
+const commitHash = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || '';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {commitHash && (
+        <div style={{
+          position: 'fixed',
+          top: 8,
+          left: 8,
+          fontSize: 12,
+          color: '#888',
+          background: '#fff8',
+          borderRadius: 4,
+          padding: '2px 8px',
+          zIndex: 9999,
+          pointerEvents: 'none'
+        }}>
+          v{commitHash.slice(0, 7)}
+        </div>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 } 
