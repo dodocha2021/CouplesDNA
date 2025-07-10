47fee94d427b4b029bd15e89ce20e7276bbe782d
dodocha <dodocha@gmail.com>
2025-07-10 12:16:12 -0700

    V1.0.6

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,10 @@
 import '../styles/animations.css';
-
-const commitHash =
-  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ||
-  process.env.VERCEL_GIT_COMMIT_SHA ||
-  '';
+import VersionTag from '../components/VersionTag';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {commitHash && (
-        <div style={{
-          position: 'fixed',
-          top: 8,
-          left: 8,
-          fontSize: 12,
-          color: '#888',
-          background: '#fff8',
-          borderRadius: 4,
-          padding: '2px 8px',
-          zIndex: 9999,
-          pointerEvents: 'none'
-        }}>
-          v{commitHash.slice(0, 7)}
-        </div>
-      )}
+      <VersionTag />
       <Component {...pageProps} />
     </>
   );
